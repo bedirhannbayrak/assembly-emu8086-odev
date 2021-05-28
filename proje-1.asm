92f2647c9e3a45ea483b97fbdee3303ac8ecc438
bedirhannbayrak <bedirhan.bayrak41@gmail.com>
2021-05-28 13:43:04 +0300

    cmt

--- proje-1.asm
+++ proje-1.asm
@@ -22,14 +22,82 @@ call ayrisayfalar
 
 call aynisayfa
 
-call sayfalariSirala   
+call sayfalariSirala
+
+; 3 - 4 - 1 - 2 
 
 
 call mouseKontrol
 
+cmp cx,80h
+jb exit
+cmp cx,97h
+ja exit 
+cmp dx,28h
+jb exit
+cmp dx,37h
+ja exit 
 
 
 
+mov dh,4  ;satir
+mov dl,12 ; sutun
+
+mov bh,4
+mov ah, 2
+int 10h
+
+call tekKareCizme ; dh,4 ; satir   
+
+
+call mouseKontrol
+
+cmp cx,0a0h
+jb exit
+cmp cx,0b7h
+ja exit 
+cmp dx,28h
+jb exit
+cmp dx,37h
+ja exit 
+
+
+
+mov dh,4  ;satir
+mov dl,16 ; sutun
+
+mov bh,4
+mov ah, 2
+int 10h
+
+call tekKareCizme ; dh,4 ; satir
+
+
+
+
+
+
+
+
+
+;cx - 80-97  kutu 1
+;dx - 28-37    
+
+;cx - a0-b7  kutu 2
+;dx - 28-37   
+
+;cx - 60-77  kutu 3
+;dx - 28-37   
+
+;cx - 40-57  kutu 3
+;dx - 28-37
+
+
+
+
+
+
+exit:
 ret ; programý kapatýr 
 
 
@@ -58,7 +126,7 @@ ret
 cizme:
 
 mov al,0 ; boþ karakter
-mov bl,0c1h ; arkaplan kirmizi
+mov bl,0e1h ; arkaplan kirmizi
 mov cx,3    ; ekrana 3 karakter yazdirir
 mov ah,09
 int 10h
@@ -69,10 +137,10 @@ ret
 
 ;----------------------------------------------- 
 
-cizmeBeyaz:
+cizmeYesil:
 
 mov al,0 ; boþ karakter
-mov bl,0ffh ; arkaplan kirmizi
+mov bl,0a1h ; arkaplan kirmizi
 mov cx,3    ; ekrana 3 karakter yazdirir
 mov ah,09
 int 10h
@@ -130,6 +198,31 @@ ret
 ;-------------------------------------------------
 
 
+
+;-----------------------------------------------
+
+tekKareCizme:
+
+mov di,2 
+
+add dl,4
+tekrar5:
+inc dh
+mov ah,2
+int 10h   ; ekranda cursor posizyonu ayarlandi
+
+call cizmeYesil
+
+dec di
+cmp di,0
+jnz tekrar5
+
+ret     
+
+
+;-------------------------------------------------
+
+
  
  
  
@@ -149,7 +242,7 @@ inc dh
 mov ah,2
 int 10h   ; ekranda cursor posizyonu ayarlandi
 
-call cizmeBeyaz
+call cizme
 
 dec di
 cmp di,0
